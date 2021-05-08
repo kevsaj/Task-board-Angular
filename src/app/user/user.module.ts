1e17326d4134e301fae37ce0d6805c9d52360cd5
Kevin Sajan <kevsaj@gmail.com>
2021-05-07 23:52:08 -0400

    email login init

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { GoogleSigninDirective } from './google-signin.directive';
+import { EmailLoginComponent } from './email-login/email-login.component';
 
 
 @NgModule({
   declarations: [
     LoginPageComponent,
-    GoogleSigninDirective
+    GoogleSigninDirective,
+    EmailLoginComponent
   ],
   imports: [
     CommonModule,
