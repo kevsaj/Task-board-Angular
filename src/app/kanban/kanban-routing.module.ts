f750cb9d03be46e33812698cc1ed2d74862a3e0e
Kevin Sajan <kevsaj@gmail.com>
2021-05-10 15:00:50 -0400

    kanban module

--- src/app/kanban/kanban-routing.module.ts
+++ src/app/kanban/kanban-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'kanban',
+    loadChildren: () =>
+      import('./kanban/kanban.module').then(m => m.KanbanModule),
+    canActivate: [AuthGuard]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
