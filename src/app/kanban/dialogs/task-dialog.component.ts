013e2697a88e0b1ef4f80a35fd1c5be7235cc91d
Kevin Sajan <kevsaj@gmail.com>
2021-05-19 23:28:07 -0400

    task dialog update

--- src/app/kanban/dialogs/task-dialog.component.ts
+++ src/app/kanban/dialogs/task-dialog.component.ts
@@ -1,20 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BoardService } from '../board.service';
 
 @Component({
   selector: 'app-task-dialog',
   template: `
-    <p>
-      task-dialog works!
-    </p>
+  <h1 mat-dialog-title>Task</h1>
+  <div mat-dialog-content class="content">
+    <mat-form-field>
+      <textarea
+        placeholder="Task description"
+        matInput
+        [(ngModel)]="data.task.description"
+      ></textarea>
+    </mat-form-field>
+    <br />
+    <mat-button-toggle-group
+      #group="matButtonToggleGroup"
+      [(ngModel)]="data.task.label"
+    >
+      <mat-button-toggle *ngFor="let opt of labelOptions" [value]="opt">
+        <mat-icon [ngClass]="opt">{{
+          opt === 'gray' ? 'check_circle' : 'lens'
+        }}</mat-icon>
+      </mat-button-toggle>
+    </mat-button-toggle-group>
+  </div>
+  <div mat-dialog-actions>
+    <button mat-button [mat-dialog-close]="data" cdkFocusInitial>
+      {{ data.isNew ? 'Add Task' : 'Update Task' }}
+    </button>
+
+  </div>
   `,
-  styles: [
-  ]
+  styleUrls: ['./task-dialog.component.scss']
 })
-export class TaskDialogComponent implements OnInit {
+export class TaskDialogComponent {
 
-  constructor() { }
+  labelOptions = ['purple', 'blue', 'green', 'yellow', 'red', 'gray'];
 
-  ngOnInit(): void {
+  constructor(
+    public dialogRef: MatDialogRef<TaskDialogComponent>,
+    private ps: BoardService,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
+  handleTaskDelete() {
+    this.ps.removeTask(this.data.boardId, this.data.task);
+    this.dialogRef.close();
+  }
 }
