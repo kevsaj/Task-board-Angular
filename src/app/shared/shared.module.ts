5e4ab4e1a97e1589812783497bfde2f859f61165
Kevin Sajan <kevsaj@gmail.com>
2021-05-22 15:40:34 -0400

    delete button component init

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ShellComponent } from './shell/shell.component';
+import { DeleteButtonComponent } from './delete-button/delete-button.component';
 
 const components = [ShellComponent];
 
@@ -35,7 +36,7 @@ const modules = [
 ];
 
 @NgModule({
-  declarations: [...components],
+  declarations: [...components, DeleteButtonComponent],
   imports: [...modules],
   exports: [
     ...components,
