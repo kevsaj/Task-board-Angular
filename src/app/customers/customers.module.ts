7997d0b307f9f497039de9cc1649e070d54a51dc
Kevin Sajan <kevsaj@gmail.com>
2021-05-25 22:07:42 -0400

    dynamic routing to detail page

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
+import { DetailPageComponent } from './detail-page/detail-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailPageComponent
+  ],
   imports: [
     CommonModule,
     CustomersRoutingModule
