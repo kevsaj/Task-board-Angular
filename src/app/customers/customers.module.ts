c027760df77ef6c28dd5c40f6fb5549c9675ef88
Kevin Sajan <kevsaj@gmail.com>
2021-05-25 22:08:23 -0400

    routing to list page

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { DetailPageComponent } from './detail-page/detail-page.component';
+import { ListPageComponent } from './list-page/list-page.component';
 
 
 @NgModule({
   declarations: [
-    DetailPageComponent
+    DetailPageComponent,
+    ListPageComponent
   ],
   imports: [
     CommonModule,
