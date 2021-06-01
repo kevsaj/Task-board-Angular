95263d359fc3ca38e3dcf67758e94f347c38de44
Kevin Sajan <kevsaj@gmail.com>
2021-05-31 23:12:35 -0400

    listpage component fucntion

--- src/app/customers/list-page/list-page.component.ts
+++ src/app/customers/list-page/list-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SeoService } from 'src/app/services/seo.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-list-page',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-page.component.scss']
 })
 export class ListPageComponent implements OnInit {
+  customers;
 
-  constructor() { }
+  constructor(private seo: SeoService, private db: AngularFirestore) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.seo.generateTags({
+      title: 'Customer List',
+      description: 'A list filled with customers'
+    });
+
+    this.customers = this.db.collection('customers').valueChanges({ idField: 'id' });
+
   }
-
 }
