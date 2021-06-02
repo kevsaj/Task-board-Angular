98d7bb9bea142657db80c92798efdec8600ccf73
Kevin Sajan <kevsaj@gmail.com>
2021-06-02 18:29:20 -0400

    server main update

--- server/main.ts
+++ server/main.ts
@@ -1,19 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ApplicationModule);
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT || 8080); // <-- update this line
 }
-
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
-declare const __non_webpack_require__: NodeRequire;
-const mainModule = __non_webpack_require__.main;
-const moduleFilename = (mainModule && mainModule.filename) || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-  bootstrap().catch(err => console.error(err));
-}
-
+bootstrap();
